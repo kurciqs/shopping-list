2f2221c17c44cb561de7512abf34fb34f370cfaa
kurciqs <90471896+kurciqs@users.noreply.github.com>
2024-11-29 11:35:28 +0100

    first release

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <h2>Nova polozka</h2>
+  <input v-model="input">
+	<button @click="addItem()">Add to list</button>
+  
+  <h2>Polozky</h2>
+	<ul>	
+    <li v-for="item in validItems" :key="`item-${item.id}`">
+      <span @click="deleteItem(item)" style="margin-right: 15px"> X </span>
+      {{item.text}}
+    </li>
+	</ul>
+
+  <h2>Zmazane Polozky</h2>
+	<ul>	
+    <li v-for="item in deletedItems" :key="`item-${item.id}`">
+      <s>
+        {{item.text}}
+      </s>
+    </li>
+	</ul>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	data() {
+		return {
+			input: '',
+			items: []
+		}
+	},
+  methods: {
+		addItem() {
+			this.items.push({
+				id: this.items.length + 1,
+				text: this.input,
+				is_deleted: false
+			})
+			this.input = ''
+		},
+    deleteItem(item) {
+			item.is_deleted = true
+		}
+	},
+  computed: {
+		validItems() {
+			return this.items.filter(item => !item.is_deleted)
+		},
+
+		deletedItems() {
+			return this.items.filter(item => item.is_deleted)
+		}
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
